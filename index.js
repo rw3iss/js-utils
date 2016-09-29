1a1c1028f45addc924932cb9bd690c0230ceecec
Ryan Weiss <rw3iss@gmail.com>
2016-09-28 22:52:44 -0400

    Refactored.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* This file contains pending functions which should be moved to the above @rw3iss/js-utils library */
+
 var addEvent = function(object, type, callback) {
     if (object == null || typeof(object) == 'undefined') return;
     if (object.addEventListener) {
@@ -15,8 +17,66 @@ var cl = function() {
 	console.log.apply(console, arguments);
 }
 
+var serializeForm = function(form) {
+	var obj = {};
+	var elements = form.querySelectorAll( "input, select, textarea" );
+	for( var i = 0; i < elements.length; ++i ) {
+		var element = elements[i];
+		var name = element.name;
+		var value = element.value;
+
+		if( name ) {
+			obj[ name ] = value;
+		}
+	}
+
+	return JSON.stringify( obj );
+};
+
+var parseUrl = function(path) {
+	var urlSections = path.split('/');
+	urlSections = urlSections.filter(function(sectionString) {
+		return sectionString.length > 0;
+	});
+
+	var urlPath = path;
+
+	return {
+		urlSections: urlSections,
+		urlPath: urlPath
+	}
+}
+
+// Determines if the given object contains all of the given properties.
+var hasProperties = function(object, properties) {
+	for(const p of properties) {
+		if(!object.hasOwnProperty(p))
+			return false;
+	}
+
+	return true;
+}
+
+var querySelector = function(selector) {
+	var els = document.querySelectorAll(selector);
+	
+	if(els.length == 1) {
+		els = els[0];
+	}
+	return els;
+}
 
 module.exports = {
+	/* general */
+	cl: cl,
+	parseUrl: parseUrl,
+
+	/* objects */
+	hasProperties: hasProperties,
+
+	/* dom stuff */
+	e: querySelector,
 	addEvent: addEvent,
-	cl: cl
+	serializeForm: serializeForm
 }
+
